ed7eccd8cf700c63cdaf03afd8bb2a2c9620a074
Ingar Shu <ingar@carbonfive.com>
2011-08-24 17:03:02 -0700

    Handle some more events.

--- public/site.js
+++ public/site.js
@@ -27,45 +27,66 @@ $(function() {
   });
 
   function handle_event(event_name, data) {
-    if (event_name === 'create') {
-      var $note = $('#proto_note').children().clone();
-      $note.css('top', 0)
-        .css('left', 0)
-        .draggable({
-          stop: function(event, ui) {
-            send_event("update", note_data($note));
-          }
-        })
-        .click(function(e) {
-          var $form = $('form', this).toggle();
-          var $content = $('.content', this).toggle();
-          var $textarea = $('textarea', $form);
-          if ($form.is(':hidden')) {
-            $content.html($textarea.val());
-            send_event("changed", note_data(this));
-          } else {
-            $textarea.focus();
-          }
-        })
-        .attr('id', S4());
 
-      $('a.delete', $note).click(function(e) {
-        e.preventDefault();
-        delete_note($note);
-      });
-      $('#board').append($note);
-      return $note;
-    }
+    var $note = $('#' + data.guid);
+
+    return {
+
+      'create': function() {
+        if ($note.length > 0) {
+          return $note;
+        }
+        $note = $('#proto_note').children().clone();
+        $note
+          .draggable({
+            stack: '.note',
+            stop: function(event, ui) {
+              send_event("update", note_data($note));
+            }
+          })
+          .css('position', 'absolute')
+          .css('top', 0)
+          .css('left', 0)
+          .click(function(e) {
+            var $form = $('form', this).toggle();
+            var $content = $('.content', this).toggle();
+            var $textarea = $('textarea', $form);
+            if ($form.is(':hidden')) {
+              $content.html($textarea.val());
+              send_event("update", note_data(this));
+            } else {
+              $textarea.focus();
+            }
+          })
+          .attr('id', data.guid);
+
+        $('a.delete', $note).click(function(e) {
+          e.preventDefault();
+          delete_note($note);
+        });
+        $('#board').append($note);
+        return $note;
+      },
+
+      'update': function() {
+        $note.css('left', data.offset.left).css('top', data.offset.top);
+        $.trim($('.content', $note).html(data.content));
+      },
+
+      'delete': function() {
+        $('#' + data.guid).remove();
+      }
+    }[event_name](data);
   }
 
   function delete_note(elem) {
     var $note = $(elem);
     send_event("delete", note_data($note));
-    $note.remove();
+    handle_event('delete', note_data($note));
   }
 
   $('#board').dblclick(function() {
-    var $note = handle_event('create');
+    var $note = handle_event('create', { guid: S4() });
     send_event("create", note_data($note));
   });
 
